5f0488435ebadb583365c12337935b601de24f9b
Hares27 <107697629+Hares27@users.noreply.github.com>
2023-12-16 08:55:28 +0530

    Update client.js

--- client.js
+++ client.js
@@ -1,38 +1,40 @@
-const { rejects } = require("assert");
+const { readFile } = require("fs");
 const net = require("net");
 const fs = require("node:fs/promises");
 const { argv } = require("node:process");
 const path = require("path");
 
-let filereadStream;
-
 const client = net.createConnection({ port: 3000, host: "127.0.0.1" }, () => {
   console.log("Connected to server");
 });
 client.setMaxListeners(Infinity);
-
+let dataUploaded, newPercentage, uploadedPercentage;
 client.on("connect", async () => {
   const filepath = argv[2];
+
+  if (filepath) {
+    (dataUploaded = 0), (newPercentage = 0), (uploadedPercentage = 0);
+    await writeFiletoServer();
+  }
+});
+
+const getFilename = async () => {
+  const filepath = argv[2];
   const filename = path.basename(filepath);
+  return filename;
+};
+
+const writeFiletoServer = async () => {
+  const filename = await getFilename();
   client.write(`Filename:${filename}`);
-  const fileHandler = await fs.open(filename, "r");
-  const filesize = (await fileHandler.stat()).size;
-  filereadStream = fileHandler.createReadStream();
-  let dataUploaded = 0;
-  let newPercentage = 0;
-  let uploadedPercentage = 0;
+  let { filereadStream, filesize } = await getFileReadStream(filename);
   filereadStream.on("data", async (chunk) => {
     if (!client.write(chunk)) {
       filereadStream.pause();
     }
-    dataUploaded += chunk.length;
-    newPercentage = Math.floor((dataUploaded / filesize) * 100);
-    if (newPercentage != uploadedPercentage) {
-      process.stdout.moveCursor(0, -1);
-      process.stdout.clearLine(0);
-      uploadedPercentage = newPercentage;
-      console.log(`Uploaded:${uploadedPercentage}%`);
-    }
+
+    await getUploadedPercentage(chunk, filesize);
+
     client.on("drain", () => {
       filereadStream.resume();
     });
@@ -42,4 +44,21 @@ client.on("connect", async () => {
     filereadStream.close();
     client.end();
   });
-});
+};
+
+const getFileReadStream = async (filename) => {
+  const fileHandler = await fs.open(filename, "r");
+  const filesize = (await fileHandler.stat()).size;
+  filereadStream = fileHandler.createReadStream();
+  return { filereadStream, filesize };
+};
+const getUploadedPercentage = async (chunk, filesize) => {
+  dataUploaded += chunk.length;
+  newPercentage = Math.floor((dataUploaded / filesize) * 100);
+  if (newPercentage != uploadedPercentage) {
+    process.stdout.moveCursor(0, -1);
+    process.stdout.clearLine(0);
+    uploadedPercentage = newPercentage;
+    console.log(`Uploaded:${uploadedPercentage}%`);
+  }
+};
